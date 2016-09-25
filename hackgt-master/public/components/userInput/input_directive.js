a627c81b3ffdfc66c8b9e24b5a51a2ec00581b25
James Connell <jamesconnell@James-MacBook-Pro.local>
2016-09-24 21:01:31 -0400

    initial

--- hackgt-master/public/components/userInput/input_directive.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('tripApp.directives')
-.directive('inputDirective', function(){
-    return {
-        restrict: 'E',
-        templateUrl: 'components/userInput/input_view.html'
-    }
-});
